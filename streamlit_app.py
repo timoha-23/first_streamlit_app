abe356ebc91fe69ea834b52d25ddf5f444516c24
TIMUR BAYRAMOV <112784898+timoha-23@users.noreply.github.com>
2022-09-03 18:33:30 -0500

    Update streamlit_app.py
    adding emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-streamlit.title('My First streamlit App')
+streamlit.title('My First streamlit App') - 🥣 🥗 🐔 🥑🍞
 streamlit.header('Importan Topics')
 streamlit.text('Be curious')
 streamlit.text('Learn')
