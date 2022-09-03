9efad1363cf5f851733150f4d2275cbe5d74c267
TIMUR BAYRAMOV <112784898+timoha-23@users.noreply.github.com>
2022-09-03 18:31:19 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit
 streamlit.title('My First streamlit App')
 streamlit.header('Importan Topics')
 streamlit.text('Be curious')
-streamlit.header('Learn')
-streamlit.header('Have Fun')
+streamlit.text('Learn')
+streamlit.text('Have Fun')
