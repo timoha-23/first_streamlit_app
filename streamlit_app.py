7ae2bbab0f479c363277735e6efb2e604c6ddbcd
TIMUR BAYRAMOV <112784898+timoha-23@users.noreply.github.com>
2022-09-03 22:25:14 -0500

    Update streamlit_app.py
    adding pandas package

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit
+import pandas
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
 streamlit.title(' 🥣 🥗 🐔 🥑🍞')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+
+streamlit.header('Build your Smoothie')
+streamlit.dataframe(my_fruit_list)
