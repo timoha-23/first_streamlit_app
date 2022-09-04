daf8b32d003d0ba0299cb8cbbf7c3b7be3d0c843
TIMUR BAYRAMOV <112784898+timoha-23@users.noreply.github.com>
2022-09-03 23:47:19 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast)
+streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('Build your Smoothie')
 
