23329891e516e3509d930643f2945c8e01deca91
TIMUR BAYRAMOV <112784898+timoha-23@users.noreply.github.com>
2022-09-16 15:34:50 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ try:
         back_from_function = get_fruityvice_data(fruit_choice)
         streamlit.dataframe(back_from_function) # present data in a table format
      except URLError as e:
-    streamlit.error()
+               streamlit.error()
         
 # don't run anything past this line
 streamlit.stop()
