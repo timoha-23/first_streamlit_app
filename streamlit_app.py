08feb7079870194199b040a215c200b1e8f90ced
TIMUR BAYRAMOV <112784898+timoha-23@users.noreply.github.com>
2022-09-16 15:32:56 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ streamlit.dataframe(fruits_to_show)
 def get_fruityvice_data(this_fruit_choice):
      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice) # get fruit details from fruityvue api response
      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) # normalize the data
-    return fruityvice_normalized:
+     return fruityvice_normalized
     
 #new section to display fruityvue api response
 streamlit.header("Fruityvice Fruit Advice!")
