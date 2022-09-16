9e600b7b4961cc780910cf2975d92b6b524eab55
TIMUR BAYRAMOV <112784898+timoha-23@users.noreply.github.com>
2022-09-16 15:06:28 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 import pandas
+import requests
 import snowflake.connector
+from urlib.error import URLError
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -26,12 +28,13 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 
 
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice) # get fruit details from fruityvue api response
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) # normalize the data
 streamlit.dataframe(fruityvice_normalized) # present data in a table format
 
+# don't run anything past this line
+streamlit.stop()
 
 #testing snowflake connection
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
