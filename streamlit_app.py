3fe22f7c75aae609c738413a10bc5c0285f60fbd
TIMUR BAYRAMOV <112784898+timoha-23@users.noreply.github.com>
2022-09-16 11:14:55 -0500

    Update streamlit_app.py
    removed non-table data output

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,9 +24,8 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
+# get fruit details from online DB
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
-
 # normalize the data
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # present data in a table format
